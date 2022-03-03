fd1bcabb98f54eb4ad0792b3dc8193577ac1931c
B-rando1 <brandonb@wightman.ca>
2022-03-03 15:34:11 -0500

    Some cosmetic stuff

--- src/Graphing.elm
+++ src/Graphing.elm
@@ -3,16 +3,38 @@ module Graphing exposing (..)
 import GraphicSVG exposing (..)
 import GraphicSVG.EllieApp exposing (..)
 import String
+import Element.Font exposing (monospace)
 
 myShapes model =
-  List.map2 (\i txt -> text txt
+  List.map2 (\i txt -> group 
+                  [
+                    roundedRect 12 5 2
+                      |> filled darkGreen
+                      |> move (-90 + i * scaleX, 6)
+                      |> notifyTap (ClickButton (-90 + i * scaleX)),
+                    text txt
                       |> size 4
                       |> centered
                       |> filled black
                       |> move (-90 + i * scaleX, 5)
-                      |> notifyTap (ClickButton (-90 + i * scaleX)))                      
+                      |> notifyTap (ClickButton (-90 + i * scaleX))
+                  ])                      
                       [0, pi/2, pi, 3*pi/2, 2*pi]
                       ["0", "\u{03C0}/2", "\u{03C0}", "3\u{03C0}/2", "2\u{03C0}"]
+  ++
+  List.map (\i -> group 
+                  [
+                    text (String.fromFloat i)
+                      |> size 4
+                      |> centered
+                      |> filled black
+                      |> move (-95, i * scaleY - 1.3)
+                      |> notifyTap (ClickButton (-90 + i * scaleX)),
+                    line (-2, 0) (2, 0)
+                      |> outlined (solid 0.5) black
+                      |> move (-90, i * scaleY)
+                  ])                      
+                      [-1, 0, 1]
   ++
   List.map (\i -> line ((i - deltaX) * scaleX, scaleY * model.func (i - deltaX)) (i * scaleX, scaleY * model.func i)
                       |> outlined (solid 0.5) black
@@ -24,7 +46,7 @@ myShapes model =
     |> outlined (solid 0.5) black,
   line (model.posX, 0) (model.posX, -scaleY * model.func (model.posX / scaleX + 0.065))
     |> outlined (solid 0.5) green,
-  line (-100, 0) (100, 0)
+  line (-93, 0) (100, 0)
     |> outlined (solid 0.5) black
   ]
 

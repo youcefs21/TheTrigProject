80a47cad74a856535e4319ee942fe0b8e5d2f978
Geon Youn <griolu@localhost.localdomain>
2022-03-03 13:17:19 -0500

    Added quadrants and a C

--- src/Circle.elm
+++ src/Circle.elm
@@ -5,7 +5,7 @@ import GraphicSVG.EllieApp exposing (..)
 
 myShapes model = [
     unitCircle,
-    triangle model.angle
+    triangle model.angle model.quad
     ]
 
 type Quadrant = One | Two | Three | Four
@@ -15,28 +15,58 @@ xpos angle = 50 * cos (degrees angle)
 ypos angle = 50 * sin (degrees angle)
 pos angle = (xpos angle, ypos angle)
 
-triangle angle = group [
-    wedge 3 (angle / 360)
-        |> outlined (solid 0.5) darkGrey
-        |> rotate (degrees (angle / 2)),
+triangle angle quad = group [
+    -- Alpha angle
+    group [
+        let
+            alpha = case quad of
+                One -> angle
+                Two -> 180 - angle
+                Three -> angle - 180
+                Four -> 360 - angle
+
+        in
+            wedge 3 (alpha / 360)
+                |> outlined (solid 0.5) darkGrey
+                |> rotate (degrees (alpha / 2))
+                |> scaleX (if quad == Two || quad == Three then -1 else 1)
+                |> scaleY (if quad == Three || quad == Four then -1 else 1)
+    ],
+
+    -- Hypothenuse
     line org (pos angle)
         |> outlined (solid 0.5) red,
+    
+    -- Adjacent
     line org (xpos angle, 0)
         |> outlined (dashed 0.5) blue,
+
+    -- Opposite
     line (xpos angle, 0) (pos angle)
         |> outlined (dashed 0.5) green,
+
+    -- Dot
     circle 1
         |> filled red
         |> move (pos angle)
     ]
   
 unitCircle = group [
+    -- Grid
     rect 0.5 110
         |> filled grey,
     rect 110 0.5
         |> filled grey,
+
+    -- Circle
     circle 50 
-        |> outlined (solid 0.5) blue
+        |> outlined (solid 0.5) blue,
+
+    -- CAST
+    text "C"
+        |> sansserif
+        |> size 10
+        |> filled black
     ]
 
 type Msg = 
@@ -67,8 +97,8 @@ updateQuad angle =
 
 init = { 
     time = 0,
-    angle = 45,
-    quad = One }
+    angle = 235,
+    quad = Three }
 
 main : EllieAppWithTick () Model Msg
 main = ellieAppWithTick Tick { 

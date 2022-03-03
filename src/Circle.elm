22444255deede1798dac9315441910d2a9350a01
Geon Youn <griolu@localhost.localdomain>
2022-03-03 13:08:27 -0500

    Added commands and scalability

--- src/Circle.elm
+++ src/Circle.elm
@@ -3,48 +3,79 @@ module Circle exposing (..)
 import GraphicSVG exposing (..)
 import GraphicSVG.EllieApp exposing (..)
 
-myShapes model =
-  [
+myShapes model = [
     unitCircle,
-    wedge 3 (d / 360)
-      |> outlined (solid 0.5) darkGrey
-      |> rotate (degrees (d / 2)),
-    line org pos
-      |> outlined (solid 0.5) red,
-    line org (xpos, 0)
-      |> outlined (dashed 0.5) blue,
-    line (xpos, 0) pos
-      |> outlined (dashed 0.5) green,
-    circle 1
-      |> filled red
-      |> move pos
-  ]
+    triangle model.angle
+    ]
+
+type Quadrant = One | Two | Three | Four
 
 org = (0, 0)
-xpos = 50 * cos (degrees d)
-ypos = 50 * sin (degrees d)
-pos = (xpos, ypos)
-d = 45
+xpos angle = 50 * cos (degrees angle)
+ypos angle = 50 * sin (degrees angle)
+pos angle = (xpos angle, ypos angle)
+
+triangle angle = group [
+    wedge 3 (angle / 360)
+        |> outlined (solid 0.5) darkGrey
+        |> rotate (degrees (angle / 2)),
+    line org (pos angle)
+        |> outlined (solid 0.5) red,
+    line org (xpos angle, 0)
+        |> outlined (dashed 0.5) blue,
+    line (xpos angle, 0) (pos angle)
+        |> outlined (dashed 0.5) green,
+    circle 1
+        |> filled red
+        |> move (pos angle)
+    ]
   
 unitCircle = group [
     rect 0.5 110
-      |> filled grey,
+        |> filled grey,
     rect 110 0.5
-      |> filled grey,
+        |> filled grey,
     circle 50 
-      |> outlined (solid 0.5) blue
-  ]
+        |> outlined (solid 0.5) blue
+    ]
 
-type Msg = Tick Float GetKeyState
+type Msg = 
+    Tick Float GetKeyState |
+    UpdateAngle Float
 
-type alias Model = { time : Float }
+type alias Model = { 
+    time  : Float,
+    angle : Float,
+    quad  : Quadrant }
 
-update msg model = case msg of
-                     Tick t _ -> { time = t }
+update msg model = 
+    case msg of
+        Tick t _ -> 
+            ({ model | time = t }, Cmd.none)
+        UpdateAngle newAngle -> 
+            ({ model | angle = limitAngle newAngle,
+                      quad  = updateQuad model.angle }, Cmd.none)
 
-init = { time = 0 }
+limitAngle angle = 
+    if angle > 360 then limitAngle (angle - 360) else angle
 
-main = gameApp Tick { model = init, view = view, update = update, title = "Game Slot" }
+updateQuad angle =
+    if angle > 0 && angle <= 90 then One
+    else if angle > 90 && angle <= 180 then Two
+    else if angle > 180 && angle <= 270 then Three
+    else Four
+
+init = { 
+    time = 0,
+    angle = 45,
+    quad = One }
+
+main : EllieAppWithTick () Model Msg
+main = ellieAppWithTick Tick { 
+    init = \_ -> ( init, Cmd.none ), 
+    update = update, 
+    view = \model -> { title = "Unit Circle", body = view model },
+    subscriptions = \_ -> Sub.none }
 
 view model = collage 192 128 (myShapes model)
 
